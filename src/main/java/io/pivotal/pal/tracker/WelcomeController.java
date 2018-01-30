d3a61025747b62a0fa5e09256509aff29fb2f573
Angelo Miranda <angelo.miranda@mastercard.com>
2018-01-30 15:04:54 -0500

    Add manifest file

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,11 +1,13 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Value("${WELCOME_MESSAGE}")
     @GetMapping("/")
     public String sayHello() {
         return "hello";
